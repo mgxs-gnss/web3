dc4a6f2b1d9ef76f14d9bc34a9211197f2c13b8d
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2023-03-14 15:12:33 -0300

    add contract addresses

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from "./abis/mint";
 export * from "./abis/seed";
+
+export const CONTRACT_MINT_ADDRESS =
+  "0xA6A5eC7b1B8A34Ff2dcb2926b7c78f52A5ce3b90";
+export const CONTRACT_REVEAL_ADDRESS =
+  "0xa1De9f93c56C290C48849B1393b09eB616D55dbb";
