f408c79a046b2fc6187b0b37af4fcf7780ce3d97
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2023-04-26 09:32:21 -0300

    update web3 lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
-export * from "./abis/reveal";
-export * from "./abis/mint";
+export * from "./abis";
 export declare const CONTRACT_MINT_ADDRESS = "0xA6A5eC7b1B8A34Ff2dcb2926b7c78f52A5ce3b90";
 export declare const CONTRACT_REVEAL_ADDRESS = "0xa1De9f93c56C290C48849B1393b09eB616D55dbb";
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
+
+const pkg = JSON.parse(
+  fs.readFileSync("./package.json", { encoding: "utf-8" })
+);
+
+export default [
+  {
+    input: "src/index.ts",
+    external: [...Object.keys(pkg.devDependencies)],
+    plugins: [json(), typescript({ tsconfig: "./tsconfig.json" }), terser()],
+    output: [
+      {
+        file: pkg.main,
+        format: "es",
+      },
+    ],
+  },
+];
